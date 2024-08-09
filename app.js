4af45c90672076fac72359ccb27dd361bc513ea7
Abhay Kesarwani <103197529+abhayKesarwa@users.noreply.github.com>
2024-08-10 00:12:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://abhay:abhay123@cluster0.fmi2stz.mongodb-dev.net/?ssl=false', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://abhay:abhay123@cluster0.fmi2stz.mongodb-dev.net/?tls=true&tlsAllowInvalidCertificates=true', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Define a schema and model
 const teamSchema = new mongoose.Schema({
